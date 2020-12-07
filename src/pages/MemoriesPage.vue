8badeb9f0805e19ae9a72c7d0f30a929f6b57ce9
AlMuz <almuz@inbox.lv>
2020-12-07 17:44:52 +0200

    delete home add new page and stlyles

---
+++ src/pages/MemoriesPage.vue
@@ -0,0 +1,41 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>All memories</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <ion-list>
+        <ion-item>A trip to the Ventspils</ion-item>
+        <ion-item>Chiil near the lake</ion-item>
+        <ion-item>Urban tourism</ion-item>
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem
+} from '@ionic/vue'
+export default {
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem
+  }
+}
+</script>
+
+<style></style>
