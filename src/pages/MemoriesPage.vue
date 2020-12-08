0be8e208f9b77e8f976316549e3dcdf3df2cf77b
AlMuz <almuz@inbox.lv>
2020-12-08 18:02:44 +0200

    use data from vuex

--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -2,7 +2,7 @@
   <base-layout page-title="All memories">
     <ion-list>
       <ion-item
-        :router-link="`/memories/${id}`"
+        :router-link="`/memories/${memory.id}`"
         v-for="memory in memories"
         :key="memory.id"
       >
@@ -27,31 +27,9 @@ export default {
     IonThumbnail,
     IonLabel
   },
-  data() {
-    return {
-      memories: [
-        {
-          id: '1',
-          title: 'A trip to the Ventspils',
-          description: 'Nice trip',
-          image:
-            'https://upload.wikimedia.org/wikipedia/commons/thumb/6/6b/Ventspilis._Livonijos_ordino_pilis%2C_1290_m._2006-09-22.jpg/1200px-Ventspilis._Livonijos_ordino_pilis%2C_1290_m._2006-09-22.jpg'
-        },
-        {
-          id: '2',
-          title: 'Chiil near the lake',
-          description: 'Cold water',
-          image:
-            'https://i.hurimg.com/i/hdn/75/0x0/5dcd630a0f25441794d60a1c.jpg'
-        },
-        {
-          id: '3',
-          title: 'Urban tourism',
-          description: 'Cool new places',
-          image:
-            'https://www.holidify.com/images/cmsuploads/compressed/Hand-Luggage-Only-2-1_20181228121957.jpg'
-        }
-      ]
+  computed: {
+    memories() {
+      return this.$store.getters.memories
     }
   }
 }
--- src/pages/MemoryDetails.vue
+++ src/pages/MemoryDetails.vue
@@ -1,8 +1,25 @@
 <template>
-  <base-layout page-title="The details" page-default-back="/memories">
+  <base-layout
+    :page-title="memoryData ? memoryData.title : 'Loading....'"
+    page-default-back="/memories"
+  >
     <h2>The details page</h2>
+    <p v-if="memoryData">{{ memoryData.id }}</p>
   </base-layout>
 </template>
-<script></script>
+<script>
+export default {
+  data() {
+    return {
+      memoryId: this.$route.params.id
+    }
+  },
+  computed: {
+    memoryData() {
+      return this.$store.getters.memory(this.memoryId)
+    }
+  }
+}
+</script>
 
 <style></style>
