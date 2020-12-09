a049e156231175137c916f691d57ab1738c94aea
AlMuz <almuz@inbox.lv>
2020-12-09 17:07:37 +0200

    add memory page added

---
+++ src/pages/AddMemoryPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <base-layout page-title="Add a memory" page-default-back="/memories">
+    <h2>Add a memory</h2>
+  </base-layout>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style></style>
--- src/pages/MemoriesPage.vue
+++ src/pages/MemoriesPage.vue
@@ -1,15 +1,29 @@
 <template>
   <base-layout page-title="All memories">
+    <template #actions-end>
+      <ion-button router-link="/memories/add">
+        <ion-icon slot="icon-only" :icon="add"></ion-icon>
+      </ion-button>
+    </template>
     <memory-list :memories="memories"></memory-list>
   </base-layout>
 </template>
 
 <script>
+import { IonButton, IonIcon } from '@ionic/vue'
 import MemoryList from '../components/memories/MemoryList'
+import { add } from 'ionicons/icons'
 
 export default {
   components: {
+    IonButton,
+    IonIcon,
     MemoryList
+  },
+  data() {
+    return {
+      add
+    }
   },
   computed: {
     memories() {
